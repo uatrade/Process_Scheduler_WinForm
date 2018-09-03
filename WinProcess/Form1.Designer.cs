617663f46878f9b5ae41744c764fb885c164dc84
uatrade <piskovtsev@gmail.com>
2018-09-03 20:55:32 +0300

    Add project files.

---
+++ WinProcess/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace WinProcess
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(455, 29);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(305, 304);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(455, 353);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(305, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Активные процессы";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(48, 108);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(98, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Запуск задачи";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(212, 111);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 3;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(48, 29);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(98, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Указать путь";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(212, 31);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(200, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(48, 70);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(98, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Новая задача";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(209, 95);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Время запуска";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(212, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Путь к файлу";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(455, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(167, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Список запущенных процессов";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Планировщик процессов";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ WinProcess/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinProcess
+{
+    public partial class Form1 : Form
+    {
+
+        System.Threading.Timer timer;
+        System.Threading.Timer timerStartProg;
+        TimerCallback callback;
+        TimerCallback callbackStartProg;
+        bool x;
+        public Form1()
+        {
+            InitializeComponent();
+            listView1.View = View.Details;
+            listView1.Columns.Add("ProcessName");
+            listView1.Columns.Add("ProcessId");
+            listView1.GridLines = true;
+            callback = new TimerCallback(TimerMetod);
+            timer = new System.Threading.Timer(callback);
+            //timer.Change(2000, 10000);
+           
+
+            //dateTimePicker1.ShowUpDown = true;
+            dateTimePicker1.CustomFormat = "HH:mm";
+            dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+
+            callbackStartProg = new TimerCallback(TimerMetodStartProg);
+         
+            x = false; //контроль запуска по времени, если не запущено;
+
+            button2.Enabled = false;
+
+        }
+
+        public void TimerMetodStartProg(object obj)
+        {
+            TimerProg();
+        }
+
+        public void TimerProg()
+        {
+            try
+            {
+                if (DateTime.Now.ToShortTimeString() == dateTimePicker1.Text&&x==false)
+                { 
+                Process.Start(textBox1.Text);
+                    x = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        public void TimerMetod(object obj)
+        {
+            Count();
+        }
+        public void Count()
+        {
+            Process[] proc = Process.GetProcesses();
+            listView1.Items.Clear();
+            for (int i = 0; i < proc.Count(); i++)
+            listView1.Items.Add(new ListViewItem(new[] { proc[i].ProcessName, proc[i].Id.ToString()}));
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer.Change(1000, 10000);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timerStartProg = new System.Threading.Timer(callbackStartProg);
+            timerStartProg.Change(100, 200);
+        }
+
+        private void button3_Click(object sender, EventArgs e) //задание пути к файлу
+        {
+
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text= openFileDialog1.FileName;
+
+                button2.Enabled = true;
+                button3.Enabled = false;
+                //ExtractIcon(path);
+            }
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button4_Click(object sender, EventArgs e)  //сброс
+        {
+            button3.Enabled = true;
+            button2.Enabled = false;
+            x = false;
+
+            timerStartProg.Dispose();
+        }
+    }
+}
